9be013f2299433720378360f9c01d3ab301f293c
Sergei <samkorbut@gmail.com>
2023-01-31 14:05:21 +0400

    Add HW9 homeworks 9

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,14 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+    const copy = [...state]
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+return action.payload === 'up' ? copy.sort((a, b) => a.name > b.name ? 1 : -1) : copy.sort((a, b)=> a.name < b.name ? 1 : -1)
         }
         case 'check': {
-
-            return state // need to fix
+return state.filter(el => el.age >= action.payload)
         }
         default:
             return state
