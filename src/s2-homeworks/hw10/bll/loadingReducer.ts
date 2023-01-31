d6d9f5d0e6e52d603d687dffbe3249428b9fd504
Sergei <samkorbut@gmail.com>
2023-01-31 20:01:15 +0400

    Revert "Add HW10 homeworks 10"
    This reverts commit ab5c3fa066f16269b4df3637be66544cf59fa990.

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,14 +1,9 @@
-const initState: initStateType = {
-    isLoading: false
+const initState = {
+    isLoading: false,
 }
 
-export type initStateType = {
-    isLoading: boolean
-}
-export const loadingReducer = (state = initState, action: LoadingActionType): initStateType => { // fix any
+export const loadingReducer = (state = initState, action: any): any => { // fix any
     switch (action.type) {
-        case 'CHANGE_LOADING':
-            return {...state, isLoading: action.isLoading}
         // пишет студент  // need to fix
 
         default:
